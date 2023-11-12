98b94722b06d66bf0c051284cf1704102c71c961
Miha <aeidom@dhbw-loerrach.de>
2023-11-13 00:32:08 +0100

    Automatically add to favourites list

--- script_currency.js
+++ script_currency.js
@@ -107,54 +107,62 @@ icon.addEventListener("click", () => {
 });
 
 // save result// Neue Funktion zum Anzeigen der gespeicherten Konvertierungen unter saved-conversions-container
+function createCheckbox(i) {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = `conversion-${i}`;
+
+  const label = document.createElement("label");
+  label.setAttribute("for", `conversion-${i}`);
+
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("height", "24px");
+  svg.setAttribute("width", "24px");
+  svg.setAttribute("viewBox", "0 0 24 24");
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", "M9.362,9.158c0,0-3.16,0.35-5.268,0.584c-0.19,0.023-0.358,0.15-0.421,0.343s0,0.394,0.14,0.521    c1.566,1.429,3.919,3.569,3.919,3.569c-0.002,0-0.646,3.113-1.074,5.19c-0.036,0.188,0.032,0.387,0.196,0.506    c0.163,0.119,0.373,0.121,0.538,0.028c1.844-1.048,4.606-2.624,4.606-2.624s2.763,1.576,4.604,2.625    c0.168,0.092,0.378,0.09,0.541-0.029c0.164-0.119,0.232-0.318,0.195-0.505c-0.428-2.078-1.071-5.191-1.071-5.191    s2.353-2.14,3.919-3.566c0.14-0.131,0.202-0.332,0.14-0.524s-0.23-0.319-0.42-0.341c-2.108-0.236-5.269-0.586-5.269-0.586    s-1.31-2.898-2.183-4.83c-0.082-0.173-0.254-0.294-0.456-0.294s-0.375,0.122-0.453,0.294C10.671,6.26,9.362,9.158,9.362,9.158z");
+
+  svg.appendChild(path);
+  label.appendChild(svg);
+
+  return { checkbox, label };
+}
+
 function displaySavedConversions() {
-    const savedConversions = JSON.parse(localStorage.getItem("savedConversions")) || [];
+  const savedConversions = JSON.parse(localStorage.getItem("savedConversions")) || [];
+  savedConversionsList.innerHTML = ""; // Leere die vorhandene Liste
 
-    // Leere die vorhandene Liste
-    savedConversionsList.innerHTML = "";
+  if (savedConversions.length > 0) {
+      const heading = document.createElement("h3");
+      heading.textContent = "Deine Gespeicherten Konvertierungen";
+      savedConversionsList.appendChild(heading);
 
-    if (savedConversions.length > 0) {
-        // Füge die Überschrift hinzu
-        const heading = document.createElement("h3");
-        heading.textContent = "Deine Gespeicherten Konvertierungen";
-        savedConversionsList.appendChild(heading);
+      savedConversions.forEach((conversion, i) => {
+          const { checkbox, label } = createCheckbox(i);
 
-        // Iteriere über die gespeicherten Konvertierungen und füge sie zur Liste hinzu
-        savedConversions.forEach(({ result, checked }) => {
-            // create a checkbox element
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            checkbox.checked = checked; // Setze den Status basierend auf der gespeicherten Information
+          label.innerHTML += `${conversion.inputAmount} ${conversion.from} = ${conversion.convertedAmount} ${conversion.to}`;
 
-            // create a label element
-            const label = document.createElement("label");
-            label.appendChild(document.createTextNode(result));
+          const listItem = document.createElement("li");
+          listItem.appendChild(checkbox);
+          listItem.appendChild(label);
+          savedConversionsList.appendChild(listItem);
 
-            // create a list item element
-            const listItem = document.createElement("li");
-            listItem.appendChild(checkbox);
-            listItem.appendChild(label);
+          checkbox.checked = conversion.checked; // Setze den Status basierend auf der gespeicherten Information
 
-            // append the list item to the saved conversions list
-            savedConversionsList.appendChild(listItem);
+          checkbox.addEventListener("click", () => {
+              updateLocalStorage(savedConversionsList);
+          });
+      });
 
-            // handle checkbox click to update localStorage
-            checkbox.addEventListener("click", () => {
-                updateLocalStorage(savedConversionsList);
-            });
-        });
+      savedConversionsList.style.display = "block";
+  } else {
+      const heading = document.createElement("h3");
+      heading.textContent = "Dein Speicherliste ist leer";
+      savedConversionsList.appendChild(heading);
 
-        // Anzeige der saved-conversions-container
-        savedConversionsContainer.style.display = "block";
-    } else {
-        // Füge eine Überschrift hinzu, wenn die Liste leer ist
-        const heading = document.createElement("h3");
-        heading.textContent = "Dein Speicherliste ist leer";
-        savedConversionsList.appendChild(heading);
-
-        // Verstecke die saved-conversions-container, wenn es keine gespeicherten Elemente gibt
-        savedConversionsContainer.style.display = "none";
-    }
+      savedConversionsList.style.display = "none";
+  }
 }
 
 // Anzeige der gespeicherten Konvertierungen beim Laden der Seite
@@ -208,6 +216,8 @@ favoritButton.addEventListener("click", () => {
 // function to update localStorage based on checkbox clicks
 function updateLocalStorage(list) {
   const savedConversions = [];
+  const favoritList = document.querySelector(".Favorit_conversions-list");
+
   list.childNodes.forEach((item) => {
       const checkbox = item.querySelector("input[type='checkbox']");
       const label = item.querySelector("label");
@@ -217,8 +227,47 @@ function updateLocalStorage(list) {
               result: label.textContent,
               checked: checkbox.checked
           });
+
+          // Überprüfe, ob das Element ausgewählt ist und nicht bereits in der Favoritenliste vorhanden ist
+          if (checkbox.checked && !isInFavoritList(label.textContent)) {
+              const favoritItem = document.createElement("li");
+              const favoritLabel = document.createElement("label");
+              favoritLabel.appendChild(document.createTextNode(label.textContent));
+              favoritItem.appendChild(favoritLabel);
+              favoritList.appendChild(favoritItem);
+          } else if (!checkbox.checked && isInFavoritList(label.textContent)) {
+              // Wenn das Element nicht ausgewählt ist und in der Favoritenliste vorhanden ist, entferne es
+              removeItemFromFavoritList(label.textContent);
+          }
       }
   });
 
   localStorage.setItem("savedConversions", JSON.stringify(savedConversions));
+}
+
+// Überprüfe, ob das Element bereits in der Favoritenliste vorhanden ist
+function isInFavoritList(result) {
+  const favoritList = document.querySelector(".Favorit_conversions-list");
+  const favoritLabels = favoritList.querySelectorAll("label");
+
+  for (const favoritLabel of favoritLabels) {
+      if (favoritLabel.textContent === result) {
+          return true;
+      }
+  }
+
+  return false;
+}
+
+// Entferne das Element aus der Favoritenliste
+function removeItemFromFavoritList(result) {
+  const favoritList = document.querySelector(".Favorit_conversions-list");
+  const favoritLabels = favoritList.querySelectorAll("label");
+
+  for (const favoritLabel of favoritLabels) {
+      if (favoritLabel.textContent === result) {
+          favoritList.removeChild(favoritLabel.parentElement);
+          break;
+      }
+  }
 }
