749fd21b7f0b9e2921f8682552a10ae3bb6b8fbd
Vae Tiolamon <72586932+Vae2002@users.noreply.github.com>
2023-11-26 19:50:12 +0100

    fixed the bug in the script_future.js file

--- script_future.js
+++ script_future.js
@@ -6,9 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
     calculateButton.addEventListener("click", function () {
         const initialInvestment = parseFloat(document.getElementById("initial-investment").value);
         const period = parseInt(document.getElementById("period").value);
-        const periodicDeposit = parseInt(document.getElementById("periodic-deposit").value);
+        const periodicDeposit = parseInt(document.getElementById("periodic-deposit-input").value);
         let periodicInterestRateFuture;
-        let compoundPeriod;
 
         if (document.getElementById("manual-interest-rate").checked) {
             periodicInterestRateFuture = parseFloat(document.getElementById("periodic-interest-rate-manual-future").value);
@@ -25,24 +24,19 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 });
 
-    });
-
     // FIX THE CALCULATION BETWEEN THE MANUAL AND AUTOMATIC periodic INTEREST RATE
     function calculateFutureValue(initialInvestment, periodicInterestRateFuture, period, periodicDeposit) {
         const compoundFrequency = parseFloat(document.getElementById("compound-frequency-present").value);
         const periodicInterestRate = periodicInterestRateFuture / compoundFrequency;
         const periodicTime = period * compoundFrequency;
-        const beginningPeriodicDeposit = document.getElementById("beginning-of-period").checked;
-        const endPeriodicDeposit = document.getElementById("end-of-period").checked;
         const decrementPeriodicDepositEnd = period - 1;
-        const compoundCheckbox = document.getElementById("compound-checkbox").checked;
         
         // Calculate future value with periodic deposits
         let futureValue;
-        if (compoundCheckbox) {
-            if (periodicDeposit > 0 && beginningPeriodicDeposit) {
+        if (document.getElementById("compound-checkbox").checked) {
+            if (periodicDeposit > 0 && document.getElementById("beginning-of-period").checked) {
                 futureValue = initialInvestment * Math.pow(1 + periodicInterestRate, periodicTime) + (periodicDeposit * (Math.pow(1 + periodicInterestRate, periodicTime) - 1)) / periodicInterestRate;
-            } else if (periodicDeposit > 0 && endPeriodicDeposit) {
+            } else if (periodicDeposit > 0 && document.getElementById("end-of-period").checked) {
                 futureValue = initialInvestment * Math.pow(1 + periodicInterestRate, periodicTime) + (decrementPeriodicDepositEnd * (Math.pow(1 + periodicInterestRate, periodicTime) - 1)) / periodicInterestRate + periodicDeposit;
             }
             else {
